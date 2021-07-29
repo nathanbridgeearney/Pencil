abd5edcd0c3803fb396aa9b744cf920dc21b550e
Nathan <nathanbridgeearney@gmail.com>
2021-07-29 23:34:37 +1200

    1

--- Pencil.java
+++ Pencil.java
@@ -9,19 +9,44 @@
  */
 
 import ecs100.*;
+
+import java.awt.*;
 import java.util.*;
 
-/** Pencil   */
-public class Pencil{
+/**
+ * Pencil
+ */
+public class Pencil {
+
     private double lastX;
     private double lastY;
+
+    private Stack<Strokes> history = new Stack<Strokes>();
+
+
+    public void undo() {
+        if (!history.empty()) {
+            history.pop();
+            UI.clearGraphics();
+            for (int i = 0; i < history.size(); i++) {
+                Strokes temp = history.pop();
+                UI.drawLine(temp.lastx, temp.lasty,temp.nx, temp.ny);
+            }
+
+        } else {
+            UI.println("test");
+        }
+
+    }
+
 
     /**
      * Setup the GUI
      */
-    public void setupGUI(){
+    public void setupGUI() {
         UI.setMouseMotionListener(this::doMouse);
         UI.addButton("Quit", UI::quit);
+        UI.addButton("Undo", this::undo);
         UI.setLineWidth(3);
         UI.setDivider(0.0);
     }
@@ -30,21 +55,24 @@ public class Pencil{
      * Respond to mouse events
      */
     public void doMouse(String action, double x, double y) {
-        if (action.equals("pressed")){
+        if (action.equals("pressed")) {
             lastX = x;
             lastY = y;
-        }
-        else if (action.equals("dragged")){
+
+        } else if (action.equals("dragged")) {
             UI.drawLine(lastX, lastY, x, y);
             lastX = x;
             lastY = y;
-        }
-        else if (action.equals("released")){
+            history.push(new Strokes(lastX, lastY, x, y));
+
+        } else if (action.equals("released")) {
             UI.drawLine(lastX, lastY, x, y);
+            history.push(new Strokes(lastX, lastY, x, y));
         }
+
     }
 
-    public static void main(String[] arguments){
+    public static void main(String[] arguments) {
         new Pencil().setupGUI();
     }
 
---
+++ Strokes.java
@@ -0,0 +1,23 @@
+import ecs100.UI;
+
+public class Strokes {
+    double lastx;
+    double lasty;
+    double nx;
+    double ny;
+
+
+    public Strokes(double lastX, double lastY, double x, double y) {
+        lastx = lastX;
+        lasty = lastY;
+        nx = x;
+        ny=y;
+
+    }
+
+
+    /**
+     * Constructor
+     */
+
+}
